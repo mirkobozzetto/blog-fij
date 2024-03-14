0ef1d833ed3a369be1b9bbbaf80343e6083d6f6d
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-14 16:41:30 +0100

    Mise à jour des variables et des boucles dans les fichiers myvars.php et loop.php

--- app/public/wp-content/themes/FIJ_blog/myvars.php
+++ app/public/wp-content/themes/FIJ_blog/myvars.php
@@ -1,10 +1,11 @@
 <?php
-$headerID = get_page_by_path( 'header' )->ID;
-$headerID = get_page_by_path( 'footer' )->ID;
-$notreasblID = get_page_by_path( 'notre-asbl' )->ID;
+$headerID = get_page_by_path('header')->ID;
+$headerID = get_page_by_path('footer')->ID;
+$notreasblID = get_page_by_path('notre-asbl')->ID;
+$contactID = get_page_by_path('contact')->ID;
 
 ?>
 
 <!-- remplir les champs ACF  -->
-<!-- <?php echo get_field( '', $headerID );
-?> -->
+<!-- <?php echo get_field('', $headerID);
+      ?> -->
