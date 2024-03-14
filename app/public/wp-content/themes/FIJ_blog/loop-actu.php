cacb6a138952d1e761f4a1d3ad86e38ce45d5f73
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-14 13:59:09 +0100

    Ajout de la section Actualité avec des informations sur les journées formations, les journées info, le petit déjeuner SASLS et les projets en cours.

---
+++ app/public/wp-content/themes/FIJ_blog/loop-actu.php
@@ -0,0 +1,213 @@
+<div id="actu" class="container-fluid">
+  <div class="row mt10 mb-5">
+    <div class="col-1 bg-bleu-tur"></div>
+    <div class="col-2 bg-bleu-tur   pt-1 pb-1 fontwhite">
+      <h3>Actualité</h3>
+    </div>
+  </div>
+
+
+  <div class="row mb-5">
+    <div class="col-10 offset-1 d-flex justify-content-around">
+
+      <div class="w-25 text-center">
+        <a href="#journée"><img class="w-100" src="pics/actu-formation.png" alt=""></a>
+        <h5>Journées formations </h5>
+        <p class="date">Juin 2022 </p>
+      </div>
+
+      <div class="w-25 text-center">
+        <a href="#info"><img class="w-100" src="pics/actu-info.png" alt=""></a>
+        <h5>Journées info </h5>
+        <p class="date">Avril 2022 </p>
+      </div>
+
+
+      <div class="w-25 text-center">
+        <a href="#déjeuner"><img class="w-100" src="pics/actu-dejeuner.png" alt=""></a>
+        <h5>Petit déjeuner SASLS </h5>
+        <p class="date">Mars 2022 </p>
+      </div>
+
+
+      <div class="w-25 text-center">
+        <a href="#projet"><img class="w-100" src="pics/actu-projet.png" alt=""></a>
+        <h5>Projets en cours  </h5>
+        <p class="date">Arnaud dupont juin 2022 </p>
+      </div>
+
+
+    </div>
+  </div>
+</div>
+
+<div id="journée" class="container-fluid bg-bleu-tur padtop mt10 padbot">
+
+  <div class="row">
+    <div class="col-1 bg-white"></div>
+    <div class="col-3 bg-white   pt-1 pb-1 ">
+      <h3>Journées formations </h3>
+    </div>
+  </div>
+
+  <div class="row mb-5 mt10">
+
+    <div class="col-5 offset-1  fontwhite d-flex align-items-center  ">
+      <div class="w80">
+        <h4 class="">Formation</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Impedit, facere? Exercitationem quam minus optio,
+          tempore cumque, modi ea officia deserunt at eveniet possimus non, similique voluptatum ducimus eligendi
+          quaerat veniam! </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Dolorem provident cum debitis dignissimos optio
+          praesentium iure eos nisi sint sequi, alias voluptatem, blanditiis consectetur, ullam facilis. Incidunt,
+          modi exercitationem cupiditate.</p>
+
+        <p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Porro pariatur veritatis vero incidunt.
+          Inventore, quibusdam, ea mollitia saepe eius rerum minus atque voluptatem. Nam quis aspernatur quibusdam
+          nisi cupiditate veritatis?</p>
+
+      </div>
+    </div>
+
+    <div class="col-5 d-flex align-items-center justify-content-center">
+      <div class="w-100 d-flex flex-column align-items-end justify-content-end">
+        <img class="w-100" src="pics/actu-formation-rect.png" alt="">
+        <a href="#actu"><i class="fa-solid fa-turn-up fontwhite  "></i></a>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+<div id="info" class="container-fluid  mt10 padbot">
+
+  <div class="row">
+    <div class="col-1 bg-bleu-tur"></div>
+    <div class="col-4 bg-bleu-tur  fontwhite pt-1 pb-1 ">
+      <h3>Journées info </h3>
+    </div>
+  </div>
+
+  <div class="row  mt10">
+
+
+
+
+    <div class="col-5 offset-1 font-black ">
+      <div class="w-100 d-flex flex-column align-items-start justify-content-start">
+        <img class="w-100" src="pics/actu-info-rect.png" alt="">
+        <a href="#actu"><i class="fa-solid fa-turn-up fa-flip-horizontal  "></i></a>
+      </div>
+    </div>
+
+
+
+
+    <div class="col-5 offset-1 d-flex align-items-center   ">
+      <div class="w80">
+        <h4 class="">Journées info</h4>
+        <p>Aide à la récolte de documents pour les locataires en difficulté dans le cadre de la révision annuelle des
+          loyers </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptas nostrum enim ex facere quasi
+          autem. Cumque fuga nam tempora earum quas consequuntur, esse veritatis, mollitia sint minima quisquam, qui.
+        </p>
+
+        <P>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Quod et laboriosam necessitatibus dicta rerum
+          quaerat error. Animi assumenda tempora vel placeat aperiam laborum necessitatibus, voluptates, cum odit,
+          dignissimos dolores eligendi.</P>
+
+
+      </div>
+    </div>
+
+
+  </div>
+
+</div>
+
+
+<div id="déjeuner" class="container-fluid bg-bleu-tur padtop mt10 padbot">
+
+  <div class="row">
+    <div class="col-1 bg-white"></div>
+    <div class="col-3 bg-white   pt-1 pb-1 ">
+      <h3>Petit déjeuner SASLS </h3>
+    </div>
+  </div>
+
+  <div class="row mb-5 mt10">
+
+    <div class="col-5 offset-1  fontwhite d-flex align-items-center  ">
+      <div class="w80">
+        <h4 class="">Petit déjeuner SASLS </h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Impedit, facere? Exercitationem quam minus optio,
+          tempore cumque, modi ea officia deserunt at eveniet possimus non, similique voluptatum ducimus eligendi
+          quaerat veniam! </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Dolorem provident cum debitis dignissimos optio
+          praesentium iure eos nisi sint sequi, alias voluptatem, blanditiis consectetur, ullam facilis. Incidunt,
+          modi exercitationem cupiditate.</p>
+
+        <p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Porro pariatur veritatis vero incidunt.
+          Inventore, quibusdam, ea mollitia saepe eius rerum minus atque voluptatem. Nam quis aspernatur quibusdam
+          nisi cupiditate veritatis?</p>
+
+      </div>
+    </div>
+
+    <div class="col-5 d-flex align-items-center justify-content-center ">
+      <div class="w-100 d-flex flex-column align-items-end justify-content-end">
+        <img class="w-100" src="pics/actu-dejeuner-rect.png" alt="">
+        <a href="#actu"><i class="fa-solid fa-turn-up fontwhite  "></i></a>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+<div id="projet" class="container-fluid padtop mt10 padbot">
+
+  <div class="row">
+    <div class="col-1 bg-bleu-tur"></div>
+    <div class="col-4 bg-bleu-tur  fontwhite pt-1 pb-1 ">
+      <h3>Projets en cours  </h3>
+    </div>
+  </div>
+
+  <div class="row mb-5 mt10">
+
+
+
+
+    <div class="col-5 offset-1 font-black ">
+      <div class="w-100 d-flex flex-column align-items-start justify-content-start">
+        <img class="w-100" src="pics/actu-projet-rect.png" alt="">
+        <a href="#actu"><i class="fa-solid fa-turn-up fa-flip-horizontal  "></i></a>
+      </div>
+    </div>
+
+
+
+
+    <div class="col-5 offset-1 d-flex align-items-center   ">
+      <div class="w80">
+        <h4 class="">Projets en cours  </h4>
+        <p>Aide à la récolte de documents pour les locataires en difficulté dans le cadre de la révision annuelle des
+          loyers </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus voluptas nostrum enim ex facere quasi
+          autem. Cumque fuga nam tempora earum quas consequuntur, esse veritatis, mollitia sint minima quisquam, qui.
+        </p>
+
+        <P>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Quod et laboriosam necessitatibus dicta rerum
+          quaerat error. Animi assumenda tempora vel placeat aperiam laborum necessitatibus, voluptates, cum odit,
+          dignissimos dolores eligendi.</P>
+      </div>
+    </div>
+  </div>
+</div>
