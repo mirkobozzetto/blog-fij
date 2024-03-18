f6112f5bf263c209cee598df6c6d868633680da7
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-18 10:59:19 +0100

    Ajouter le fichier loop-travail_as.php et commenter la ligne travail_asID dans myvars.php

--- app/public/wp-content/themes/FIJ_blog/myvars.php
+++ app/public/wp-content/themes/FIJ_blog/myvars.php
@@ -3,6 +3,7 @@ $headerID = get_page_by_path('header')->ID;
 $footerID = get_page_by_path('footer')->ID;
 $notreasblID = get_page_by_path('notre-asbl')->ID;
 $contactID = get_page_by_path('contact')->ID;
+// $travail_asID = get_page_by_path('travail_as')->ID;
 ?>
 
 <!-- remplir les champs ACF  -->
