9c014ccc6218b2c42fa20074c658cb896ca34f78
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-26 10:13:10 +0100

    Mises à jour du plugin Safe SVG*** Les fichiers .DS_Store ont été supprimés du répertoire du plugin Safe SVG. Le fichier style.css a été modifié pour ajouter une nouvelle classe .mb10 avec une marge inférieure de 10rem. Les fichiers safe-svg-admin.asset.php et safe-svg-block.asset.php ont été modifiés pour mettre à jour les versions des dépendances. Le fichier installed.php a été modifié pour mettre à jour la version du plugin. Le fichier readme.txt a été modifié pour mettre à jour la version testée de WordPress.

--- app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
+++ app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
@@ -1,1 +1,110 @@
+<div class="container-fluid">
 
+  <div class="row mt10 mb-5">
+    <div class="col-1 bg-bleu-tur"></div>
+    <div class="col-3 bg-bleu-tur   pt-1 pb-1 fontwhite">
+      <h3>Les jobs disponibles</h3>
+    </div>
+  </div>
+
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>Assistant(e) social(e) - Assistant(e) en psychologie</h3>
+      <h4>Anderlecht <br>
+        Temps plein, CDI</h4>
+      <p>
+        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
+        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
+        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
+        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
+        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
+        délivrée et sur la recherche constante de nouvelles activités
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>Office Assistant & Social Fund Officer</h3>
+      <h4>Schaerbeek <br>
+        Temps plein, CDI</h4>
+      <p>
+        Beantwoorden van vragen en verstrekken van informatie/assistentie.
+        Mailings organiseren om persoonlijke gegevens van werknemers te verzamelen.
+        Annonce sponsorisée·PostedOffre publiée il y a plus de 30 jours
+
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>ASSISTANTS SOCIAUX DE SECTEUR (F/H/X)</h3>
+      <h4>Bruxelles <br>
+        Temps plein, CDI</h4>
+      <p>
+        Bonne connaissance du réseau social Bruxellois.
+        Accompagné d’un assistant social de la cellule logement du CPAS, le TSR ira à la rencontre des personnes en
+        situation précaire
+
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+
+  <div class="row mt10 mb-5">
+    <div class="col-1 bg-bleu-tur"></div>
+    <div class="col-3 bg-bleu-tur   pt-1 pb-1 fontwhite">
+      <h3>Les stages disponibles</h3>
+    </div>
+  </div>
+
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>Assistant Ressources Humaines - Sous-officier</h3>
+      <h4>La défense Belgique<br>
+        Temps plein, CDI</h4>
+      <p>
+        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
+        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
+        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
+        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
+        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
+        délivrée et sur la recherche constante de nouvelles activités
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>Stage Assistant RH</h3>
+      <h4>Waterloo Skyforce Cyber Security<br>
+        Temps plein, CDI</h4>
+      <p>
+        Vous êtes à la recherche d'un stage conventionné.
+        Être un support administratif office et au RH Manager (courriers divers, documents sociaux, etc).
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+
+  <div class="row mt10 mb10">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>Assistant Ressources Humaines - Sous-officier</h3>
+      <h4>La défense Belgique<br>
+        Temps plein, CDI</h4>
+      <p>
+        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
+        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
+        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
+        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
+        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
+        délivrée et sur la recherche constante de nouvelles activités
+      </p>
+      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+    </div>
+  </div>
+</div>
