2613f7d29eb7801ccc5946e2c0688a99dc4dfd92
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-27 16:24:02 +0100

    Mise à jour de la boucle des étapes d'emploi dans le thème FIJ_blog

--- app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
+++ app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
@@ -1,32 +1,45 @@
 <div class="container-fluid">
-  <!-- <?php var_dumpj($job_stages); ?> -->
   <div class="row mt10 mb-5">
     <div class="col-1 bg-bleu-tur"></div>
     <div class="col-3 bg-bleu-tur   pt-1 pb-1 fontwhite">
-      <?php
-      var_dump(get_field("jobs"))
-      ?>
-      <h3>Les jobs disponibles</h3>
+
+      <h3>
+        <?php
+        echo get_field("jobs")
+        ?>
+      </h3>
     </div>
   </div>
-
-  <div class="row mt10 ">
-    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
-      <h3>Assistant(e) social(e) - Assistant(e) en psychologie</h3>
-      <h4>Anderlecht <br>
-        Temps plein, CDI</h4>
-      <p>
-        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
-        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
-        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
-        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
-        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
-        délivrée et sur la recherche constante de nouvelles activités
-      </p>
-      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+  <?php
+  // var_dumpj(get_field("jobs_posts"));
+  $job_list = get_field("jobs_posts");
+  foreach ($job_list as $key => $value) {
+    var_dumpj($value);
+    if (get_field('type_contrat', $value['job']) == "CDD") {
+      $contrat = get_field('type_contrat', $value['job']) . " d'une durée de " . get_field('duree', $value['job']);
+    } else {
+      $contrat = get_field('type_contrat', $value['job']);
+    }
+  ?>
+    <div class="row mt10 ">
+      <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+        <h3>Assistant(e) social(e) - Assistant(e) en psychologie</h3>
+        <h4>Anderlecht <br>
+          Temps plein, CDI</h4>
+        <p>
+          TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
+          situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
+          développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
+          gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
+          accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
+          délivrée et sur la recherche constante de nouvelles activités
+        </p>
+        <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+      </div>
     </div>
-  </div>
-
+  <?php
+  }
+  ?>
   <div class="row mt10 ">
     <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
       <h3>Office Assistant & Social Fund Officer</h3>
