ef36a0f4c2fb7f1eb21ff7bd96abb586baad96d1
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-28 14:34:30 +0100

    Mise à jour du style.css, php-fpm.log et loop.php

--- app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
+++ app/public/wp-content/themes/FIJ_blog/loop-job_stages.php
@@ -18,94 +18,95 @@
   foreach ($job_list as $key => $value) {
     // var_dumpj($value);
     if (get_field('type_contrat', $value['job']) == "CDD") {
-      $contrat = get_field('type_contrat', $value['job']) . " d'une durée de " . get_field('duree', $value['job']);
+      $contrat_job = get_field('type_contrat', $value['job']) . " d'une durée de " . get_field('duree', $value['job']);
     } else {
-      $contrat = get_field('type_contrat', $value['job']);
+      $contrat_job = get_field('type_contrat', $value['job']);
     }
   ?>
-    <div class="row mt10 ">
-      <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
-        <h3>
-          <?php
+  <div class="row mt10 ">
+    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
+      <h3>
+        <?php
           echo get_the_title($value['job']);
           ?>
-        </h3>
-        <h4>
-          <?php echo get_field('lieu', $value['job']); ?>
-          <br>
-          <?php echo get_field('regime', $value['job']); ?> , <?php echo $contrat; ?>
-        </h4>
-        <p>
-          <?php echo get_field('description', $value['job']); ?>
-        </p>
-        <?php
+      </h3>
+      <h4>
+        <?php echo get_field('lieu', $value['job']); ?>
+        <br>
+        <?php echo get_field('regime', $value['job']); ?> , <?php echo $contrat_job; ?>
+      </h4>
+      <p>
+        <?php echo get_field('description', $value['job']); ?>
+      </p>
+      <?php
         $postuler = get_field("postuler");
         // var_dumpj($postuler);
         ?>
-        <a class="btn bg-bluef fontwhite fs-5 mb-2" href="<?php echo $postuler['url']; ?>">
-          <?php echo $postuler['title']; ?>
-        </a>
-        <!-- <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a> -->
-      </div>
+      <a class="btn bg-bluef fontwhite fs-5 mb-2" href="<?php echo $postuler['url']; ?>">
+        <?php echo $postuler['title']; ?>
+      </a>
+      <!-- <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a> -->
     </div>
+  </div>
   <?php
   }
   ?>
 
+  <!--  -->
+
   <div class="row mt10 mb-5">
     <div class="col-1 bg-bleu-tur"></div>
     <div class="col-3 bg-bleu-tur   pt-1 pb-1 fontwhite">
-      <h3>Les stages disponibles</h3>
+      <h3>
+        <?php
+        echo get_field("stages")
+        ?>
+      </h3>
     </div>
   </div>
 
+  <?php
+  $stages_list = get_field("stages_posts");
+  foreach ($stages_list as $key => $value) {
+    // var_dumpj($value);
+    // if (get_field('type_contrat', $value['stage']) == "") {
+    if (get_field('duree', $value['stage']) == "") {
+      $contrat_stage = get_field('type_contrat', $value['stage']);
+    } else {
+      $contrat_stage = get_field('type_contrat', $value['stage']) . " d'une durée de " . get_field('duree', $value['stage']);
+    }
+  ?>
   <div class="row mt10 ">
     <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
-      <h3>Assistant Ressources Humaines - Sous-officier</h3>
-      <h4>La défense Belgique<br>
-        Temps plein, CDI</h4>
+      <h3>
+        <?php
+          echo get_the_title($value['stage']);
+          ?>
+      </h3>
+      <h4>
+        <?php
+          echo get_field('lieu', $value['stage']);
+          ?>
+        <br>
+        <?php
+          echo get_field('regime', $value['stage']);
+          ?> ,
+        <?php
+          echo $contrat_stage;
+          ?>
+      </h4>
       <p>
-        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
-        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
-        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
-        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
-        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
-        délivrée et sur la recherche constante de nouvelles activités
+        <?php
+          echo get_field('description', $value['stage']);
+          ?>
       </p>
-      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
+      <a class="btn bg-bluef fontwhite fs-5 mb-2" href="<?php echo $postuler['url']; ?>">
+        <?php echo $postuler['title']; ?>
+      </a>
     </div>
   </div>
-
-  <div class="row mt10 ">
-    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
-      <h3>Stage Assistant RH</h3>
-      <h4>Waterloo Skyforce Cyber Security<br>
-        Temps plein, CDI</h4>
-      <p>
-        Vous êtes à la recherche d'un stage conventionné.
-        Être un support administratif office et au RH Manager (courriers divers, documents sociaux, etc).
-      </p>
-      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
-    </div>
-  </div>
-
-  <div class="row mt10">
-    <div class="col-8 offset-1  border bg-bleu-tur fontwhite p-5">
-      <h3>Assistant Ressources Humaines - Sous-officier</h3>
-      <h4>La défense Belgique<br>
-        Temps plein, CDI</h4>
-      <p>
-        TRAVIE est une Entreprise de Travail Adapté (ETA) dont l’objet social vise l’épanouissement de la personne en
-        situation de handicap et sa structuration par le travail. La personne, sa formation, son encadrement, son
-        développement, sa sécurité et son bien-être au travail sont au centre des préoccupations de l’équipe de
-        gestion. Afin d’assurer la pérennité de cet objet social, une attention toute particulière est également
-        accordée à la rentabilité économique de l’entreprise, avec notamment un accent rigoureux sur la qualité
-        délivrée et sur la recherche constante de nouvelles activités
-      </p>
-      <a href=""><button class="btn bg-bluef fontwhite fs-5 mb-2 "> postuler</button></a>
-    </div>
-  </div>
-</div>
-
-<div class="mt10"></div>
-</main>
+  <?php
+  }
+  ?>
+  <div class="mt10"></div>
+  </main>
