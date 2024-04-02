0f5657543750842b01268e7280982572e374184f
Mirko Bozzetto <mirkouch@gmail.com>
2024-04-02 12:31:55 +0200

    Mise à jour du style.css et loop-contact.php

--- app/public/wp-content/themes/FIJ_blog/loop-contact.php
+++ app/public/wp-content/themes/FIJ_blog/loop-contact.php
@@ -3,40 +3,59 @@
   <div class="row mt10 mb-5">
     <div class="col-1 bg-bleu-tur"></div>
     <div class="col-2 bg-bleu-tur   pt-1 pb-1 fontwhite">
-      <h3>Localisation</h3>
+      <h3>
+        <?php
+        echo get_field("title")
+        ?>
+      </h3>
     </div>
   </div>
 
   <div class="row ">
 
     <div class="col-5 offset-1 padtop ">
-      <h2>SASLS ASBL </h2>
+      <h2>
+        <?php
+        echo get_field("subtitle")
+        ?>
+      </h2>
 
       <p class="fs-5">
-        Adresse: Maison du Logement - Avenue de la Toison d'Or, 72
-        1060 Saint-Gilles
+        <?php
+        $adress = get_field("adress");
+        echo $adress["adress"]
+        ?>
       </p>
 
       <p class="fs-5">
-        Tel: 02 502 88 15 
-        Mail: infodirection@sasls.irisnet.be 
+        <span>
+          <a href="tel:<?php echo $adress['phone']; ?>" style="color: black;">
+            <?php echo $adress['phone']; ?>
+          </a>
+        </span>
+        <!-- ici, une tabulation-->&emsp;
+        <!-- Mail: infodirection@sasls.irisnet.be  -->
+        Mail:
+        <span>
+          <a style="color: black; text-decoration: underline;" href="mailto:<?php echo $adress['mail']; ?>">
+            <?php echo $adress['mail']; ?>
+          </a>
+        </span>
       </p>
 
-      <p class="fs-5">
-        Vous pouvez prendre contact avec le siège de l'ASBL pendant les heures d’ouverture :<br>
-        Du lundi au vendredi : 8h30 à 12h30 et 13h30 à 15h30  
-      </p>
-
-      <p class="fs-5">
-        Accès en transport en commun <br>
-        Métro - Louise  <br>
-        Métro - Hôtel des Monnaies  
-      </p>
-
+      <div class="unique-class" style="font-size: 1.25rem !important;">
+        <?php
+        echo get_field("open_acces")
+        ?>
+      </div>
     </div>
+    <?php var_dumpj(get_fields()) ?>
 
     <div class="col-5 d-flex justify-content-center align-items-center">
       <div class="w80">
+        <?php
+        $map = get_field("map");
+        ?>
         <img class="w-100" src="pics/plan.png" alt="">
       </div>
     </div>
